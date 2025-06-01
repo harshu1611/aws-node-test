d0cef43db5e1364257a7d457db0deb2e02bcd7d1
Harsh Agrawal <harsh.mobile231@gmail.com>
2025-06-01 09:57:17 +0530

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// const router = require('./routes.js');
+const app= express();
+app.use(bodyParser.json());
+
+
+app.get('/',(req,res)=>{  
+    res.send('Welcome to AWS API ');
+});
+
+// app.use(router)
+
+app.listen(3002,()=>{
+    console.log('Server is running on port 3002');
+})
+
