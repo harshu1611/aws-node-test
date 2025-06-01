e11378919ba5912014ef247ff2b277206feecf5b
Harsh Agrawal <harsh.mobile231@gmail.com>
2025-06-01 11:26:38 +0530

    port update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/',(req,res)=>{
 
 // app.use(router)
 
-app.listen(3002,()=>{
-    console.log('Server is running on port 3002');
+app.listen(process.env.PORT,()=>{
+    console.log('Server is running on port');
 })
 
